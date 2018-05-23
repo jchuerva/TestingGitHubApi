cc9843f06e3e5252b147c662e9c7731d897a7ff3
Carlos Alarcon <jchuerva@github.com>
2018-05-23 21:48:37 +0200

    replace Octokit to Faraday for request without auth

--- step_definitions/helpers/request_helpers.rb
+++ step_definitions/helpers/request_helpers.rb
@@ -1,12 +1,3 @@
-def get_resp_body(purl)
-  conn = Faraday.new(url: purl) do |faraday|
-    faraday.response :json, parser_options: { symbolize_names: false }
-    faraday.adapter Faraday.default_adapter
-  end
-    response = conn.get
-    response.body
-end
-
 def request_user_token
   @user_token = '' if @user_token.nil? || !@valid_user_token
   cli = HighLine.new
